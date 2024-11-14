c7cfd794458cc8625a355b992633333ac91dd56d
Hari V <hariharen.veerakumar@gmail.com>
2024-11-14 15:04:22 +1100

    Commenting out with rationale why not including

--- src/write_data.py
+++ src/write_data.py
@@ -35,18 +35,21 @@ class RecordHundredData:
         # Define a new signal.
         new_measure_id = sdk.insert_measure(measure_tag=self.sig_name, freq=self.freq_hz, freq_units=FREQ_UNITS_HZ)
         
-        # EXTRA ADDED: Calculate the digital to analog scale factors.
-        segment = self.record
-        assert len(segment.adc_gain) == 2 and (segment.adc_gain[0] == segment.adc_gain[1]) # not sure why this is an array, just take first val
-        gain = segment.adc_gain[0]
-        assert len(segment.baseline) == 2 and (segment.baseline[0] == segment.baseline[1]) # not sure why this is an array, just take first val
-        baseline = segment.baseline[0]
-        scale_m = 1 / gain
-        scale_b = -baseline / gain
+        # # EXTRA ADDED: Calculate the digital to analog scale factors.
+        # # Not required since this is already an analog signal, this is only needed for digital signals
+        # segment = self.record
+        # assert len(segment.adc_gain) == 2 and (segment.adc_gain[0] == segment.adc_gain[1]) # not sure why this is an array, just take first val
+        # gain = segment.adc_gain[0]
+        # assert len(segment.baseline) == 2 and (segment.baseline[0] == segment.baseline[1]) # not sure why this is an array, just take first val
+        # baseline = segment.baseline[0]
+        # scale_m = 1 / gain
+        # scale_b = -baseline / gain
 
         # Write Data
         sdk.write_data_easy(new_measure_id, new_device_id, self.time_data, self.value_data, self.freq_hz, 
-                            scale_m=scale_m, scale_b=scale_b, freq_units=FREQ_UNITS_HZ)
+                            freq_units=FREQ_UNITS_HZ
+                            # , scale_m=scale_m, scale_b=scale_b
+                            )
 
         print("Data inserted")
         print("measure_id = ", new_measure_id)
